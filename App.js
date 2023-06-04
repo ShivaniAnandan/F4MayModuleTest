fc079075c081569d9aed7d318b43844216af2e2a
ShivaniAnandan <124684724+ShivaniAnandan@users.noreply.github.com>
2023-06-04 20:17:17 +0530

    Final commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2 style={{marginLeft:"20px"}}>Social Media App</h2>
+      <hr></hr>
+    </div>
+  );
+}
+
+export default App;
